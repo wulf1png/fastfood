5ed7265d7cd6a989af1e3f0f0935643e760a2ee8
Png Wulf <deucelives2000@gmail.com>
2019-06-13 17:28:29 +0300

    вот теперь точно все)

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -75,10 +75,10 @@
             this.longer,
             this.Cena,
             this.Data});
-            this.dataGridView1.Location = new System.Drawing.Point(428, 98);
+            this.dataGridView1.Location = new System.Drawing.Point(331, 22);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(644, 280);
+            this.dataGridView1.Size = new System.Drawing.Size(644, 608);
             this.dataGridView1.TabIndex = 7;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
